d47df20b3ae335d4b9f565e76a5804c1275a2b2f
corn1200 <corn120033@gmail.com>
2021-12-15 16:57:37 +0900

    Todo 데이터베이스 클래스 주석 추가

--- app/src/main/java/com/example/rxjavaexample/TodoRoomDatabase.java
+++ app/src/main/java/com/example/rxjavaexample/TodoRoomDatabase.java
@@ -11,12 +11,14 @@ import androidx.room.RoomDatabase;
 import androidx.room.RoomDatabase.Callback;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
+// 데이터베이스에 테이블을 추가, 데이터베이스 버전 설정
 @Database(entities = {Todo.class}, version = 2, exportSchema = false)
 public abstract class TodoRoomDatabase extends RoomDatabase {
-
+    // 데이터베이스를 정적 필드로 가짐
     public abstract TodoDao todoDao();
     private static TodoRoomDatabase INSTANCE;
 
+    // 데이터베이스 명을 지정하고 데이터베이스 생성 후 반환
     public static TodoRoomDatabase getDatabase(final Context context) {
         if (INSTANCE == null) {
             synchronized (TodoRoomDatabase.class) {
