845ca06683f33356f39bfb803e1d16de356d6e8f
corn1200 <corn120033@gmail.com>
2021-12-15 15:53:42 +0900

    Todo 뷰모델 클래스 주석 추가

--- app/src/main/java/com/example/rxjavaexample/TodoViewModel.java
+++ app/src/main/java/com/example/rxjavaexample/TodoViewModel.java
@@ -8,6 +8,7 @@ import androidx.lifecycle.LiveData;
 
 import java.util.List;
 
+// 뷰모델을 상속한다
 public class TodoViewModel extends AndroidViewModel {
     private TodoRepository mRepository;
     private LiveData<List<Todo>> mAllTodos;
@@ -18,23 +19,28 @@ public class TodoViewModel extends AndroidViewModel {
         mAllTodos = mRepository.getAllTodos();
     }
 
+    // 뷰모델 객체 내의 모든 데이터를 반환
     LiveData<List<Todo>> getAllTodos() {
         return mAllTodos;
     }
 
+    // 레포지토리를 통해 데이터 삽입 메서드 실행
     public void insert(Todo todo) {
         mRepository.insert(todo);
     }
 
+    // 레포지토리를 통해 데이터 삭제 메서드 실행
     public void delete(Todo todo) {
         mRepository.delete(todo);
     }
 
+    // 레포지토리를 통해 데이터 업데이트 메서드 실행
     public void update(Todo todo) {
         mRepository.update(todo);
     }
 
-    public void deleteAll() {
+    // 레포지토리를 통해 모든 데이터 삭제 메서드 실행행
+   public void deleteAll() {
         mRepository.deleteAll();
     }
 }
