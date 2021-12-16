37d0207b715eaf9f38e0cd7092b1e8ecdd366194
corn1200 <corn120033@gmail.com>
2021-12-16 15:28:22 +0900

    AsyncTask 쓰던 부분 RxJava 로 전환

--- app/src/main/java/com/example/rxjavaexample/TodoDao.java
+++ app/src/main/java/com/example/rxjavaexample/TodoDao.java
@@ -10,24 +10,27 @@ import androidx.room.Update;
 
 import java.util.List;
 
+import io.reactivex.rxjava3.core.Completable;
+
 // 데이터 접근 객체로 지정
 @Dao
 public interface TodoDao {
+    // Completable 은 데이터를 발행하지 않고 수행만 한 후 종료한다
     // 중복 삽입으로 인한 예외 무시
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    void insert(Todo todo);
+    Completable insert(Todo todo);
 
     // 해당 데이터를 삭제
     @Delete
-    void delete(Todo todo);
+    Completable delete(Todo todo);
 
     // 모든 데이터를 삭제
     @Query("DELETE FROM todo_table")
-    void deleteAll();
+    Completable deleteAll();
 
     // 데이터 업데이트
     @Update
-    void update(Todo... todos);
+    Completable update(Todo... todos);
 
     // 모든 데이터를 조회
     @Query("SELECT * FROM todo_table ORDER BY id ASC")
--- app/src/main/java/com/example/rxjavaexample/TodoRepository.java
+++ app/src/main/java/com/example/rxjavaexample/TodoRepository.java
@@ -7,6 +7,9 @@ import androidx.lifecycle.LiveData;
 
 import java.util.List;
 
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
 public class TodoRepository {
     private TodoDao mTodoDao;
     private LiveData<List<Todo>> mAllTodos;
@@ -25,81 +28,34 @@ public class TodoRepository {
 
     // 백그라운드에서 데이터를 삽입
     public void insert(Todo todo) {
-        new InsertAsyncTask(mTodoDao).execute(todo);
+        mTodoDao.insert(todo)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe();
     }
 
     // 백그라운드에서 데이터를 삭제
     public void delete(Todo todo) {
-        new DeleteAsyncTask(mTodoDao).execute(todo);
+        mTodoDao.delete(todo)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe();
     }
 
     // 백그라운드에서 데이터를 업데이트
     public void update(Todo todo) {
-        new UpdateAsyncTask(mTodoDao).execute(todo);
+        mTodoDao.update(todo)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe();
     }
 
     // 백그라운드에서 모든 데이터 삭제
     public void deleteAll() {
-        new DeleteAllAsyncTask(mTodoDao).execute();
+        mTodoDao.deleteAll()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe();
     }
 
-    // AsyncTask 에서 사용할 dao 를 초기화 후 파라미터 값 삽입
-    private static class InsertAsyncTask extends AsyncTask<Todo, Void, Void> {
-        private TodoDao mAsyncTaskDao;
-
-        InsertAsyncTask(TodoDao dao) {
-            mAsyncTaskDao = dao;
-        }
-
-        @Override
-        protected Void doInBackground(Todo... todos) {
-            mAsyncTaskDao.insert(todos[0]);
-            return null;
-        }
-    }
-
-    // AsyncTask 에서 사용할 dao 를 초기화 후 파라미터 값 삭제
-    private static class DeleteAsyncTask extends AsyncTask<Todo, Void, Void> {
-        private TodoDao mAsyncTaskDao;
-
-        DeleteAsyncTask(TodoDao dao) {
-            mAsyncTaskDao = dao;
-        }
-
-        @Override
-        protected Void doInBackground(Todo... todos) {
-            mAsyncTaskDao.delete(todos[0]);
-            return null;
-        }
-    }
-
-    // AsyncTask 에서 사용할 dao 를 초기화 후 데이터 전체 삭제
-    private static class DeleteAllAsyncTask extends AsyncTask<Void, Void, Void> {
-        private TodoDao mAsyncTaskDao;
-
-        DeleteAllAsyncTask(TodoDao dao) {
-            mAsyncTaskDao = dao;
-        }
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            mAsyncTaskDao.deleteAll();
-            return null;
-        }
-    }
-
-    // AsyncTask 에서 사용할 dao 를 초기화 후 데이터 업데이트
-    private static class UpdateAsyncTask extends AsyncTask<Todo, Void, Void>{
-        private TodoDao mAsyncTaskDao;
-
-        UpdateAsyncTask(TodoDao dao) {
-            mAsyncTaskDao = dao;
-        }
-
-        @Override
-        protected Void doInBackground(Todo... todos) {
-            mAsyncTaskDao.update(todos[0]);
-            return null;
-        }
-    }
 }
